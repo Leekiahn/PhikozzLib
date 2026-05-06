aa18e8f85a15da9519f9bb1bc2c4ed91f242a413
Leekiahn <kiahn2000@gmail.com>
2026-05-06 15:29:14 +0900

    ResourceManager 수정

--- Runtime/Resource/IResourceService.cs
+++ Runtime/Resource/IResourceService.cs
@@ -5,12 +5,8 @@ namespace PhikozzLib
 {
     public interface IResourceService
     {
-        T Load<T>(string key);
         Task<T> LoadAsync<T>(string key);
-        
-        IList<T> LoadAll<T>(string label);
         Task<IList<T>> LoadAllAsync<T>(string label);
-        
         void Release(string key);
         void ReleaseAll(string label);
     }
