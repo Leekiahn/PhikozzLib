521ffb64edbf72b5e56e4a811f84a611936b782a
Leekiahn <kiahn2000@gmail.com>
2026-05-11 20:55:44 +0900

    EventManager 구현

--- Runtime/Core/Core.cs
+++ Runtime/Core/Core.cs
@@ -6,6 +6,7 @@ namespace PhikozzLib
         public static ISaveService Save => ServiceLocator.Get<ISaveService>();
         public static IResourceService Resource => ServiceLocator.Get<IResourceService>();
         public static ISceneService Scene => ServiceLocator.Get<ISceneService>();
+        public static IEventService Event => ServiceLocator.Get<IEventService>();
     }
 }
 
