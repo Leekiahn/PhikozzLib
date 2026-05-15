7288abd5da41be62275197ac0e1362098a1008d8
Leekiahn <kiahn2000@gmail.com>
2026-05-15 20:05:17 +0900

    SceneService의 이벤트들 이벤트버스로 교체

--- Runtime/Scene/ISceneService.cs
+++ Runtime/Scene/ISceneService.cs
@@ -9,10 +9,6 @@ namespace PhikozzLib
         bool IsLoading { get; }
         string CurrentSceneName { get; }
 
-        event Action<string, LoadSceneMode> OnSceneLoaded;
-        event Action<string> OnSceneUnloaded;
-        event Action<string, string> OnSceneChanged;
-
         AsyncOperation LoadAsync(string sceneName, LoadSceneMode mode = LoadSceneMode.Single);
         AsyncOperation UnloadAsync(string sceneName);
         AsyncOperation ReloadAsync();
