3de9f1e86955ad8703f8c4342a26a4ffb35cf496
Leekiahn <kiahn2000@gmail.com>
2026-05-16 22:25:47 +0900

    UIManager 수정

--- Runtime/UI/UIManager.cs
+++ Runtime/UI/UIManager.cs
@@ -38,26 +38,18 @@ namespace PhikozzLib
                 return cachedUI as T;
             }
 
-            if (!_uiPrefabs.TryGetValue(type, out UIBase prefab))
+            if (_uiPrefabs.TryGetValue(type, out UIBase prefab))
             {
-                throw new InvalidOperationException(
-                    $"{type.Name} UI 프리팹이 `UIManager` 인스펙터에 등록되지 않았습니다.");
+                UIBase instance = Instantiate(prefab, _uiRoot);
+                T ui = instance.GetComponent<T>();
+
+                _openedUIs[type] = ui;
+                ui.Show();
+                
+                return ui;
             }
 
-            UIBase instance = Instantiate(prefab, _uiRoot);
-            T ui = instance.GetComponentInChildren<T>(true);
-
-            if (ui == null)
-            {
-                Destroy(instance.gameObject);
-                throw new InvalidOperationException(
-                    $"{type.Name} 타입의 UI 컴포넌트를 프리팹에서 찾을 수 없습니다.");
-            }
-
-            _openedUIs[type] = ui;
-            ui.Show();
-
-            return ui;
+            return null;
         }
 
         public void Close<T>() where T : UIBase
@@ -76,19 +68,7 @@ namespace PhikozzLib
 
             foreach (UIReference reference in _uiReferences)
             {
-                if (reference == null || reference.Prefab == null)
-                {
-                    continue;
-                }
-
                 Type type = reference.Prefab.GetType();
-
-                if (_uiPrefabs.ContainsKey(type))
-                {
-                    Debug.LogWarning($"{type.Name} UI 프리팹이 중복 등록되어 있습니다.", this);
-                    continue;
-                }
-
                 _uiPrefabs.Add(type, reference.Prefab);
             }
         }
