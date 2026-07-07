0bbea406215db079b820e716ae0fb6480baacd78
Leekiahn <kiahn2000@gmail.com>
2026-07-07 15:58:48 +0900

    TrackedPool 구현

--- Runtime/Core/Core.cs
+++ Runtime/Core/Core.cs
@@ -7,7 +7,6 @@ namespace PhikozzLib
         public static IAddressableService Addressable => ServiceLocator.Get<IAddressableService>();
         public static ISceneService Scene => ServiceLocator.Get<ISceneService>();
         public static IEventService Event => ServiceLocator.Get<IEventService>();
-        public static IPoolService Pool => ServiceLocator.Get<IPoolService>();
         public static IPopupService Popup => ServiceLocator.Get<IPopupService>();
     }
 }
