20532046657eba4b7de572a5ee4a6e0e90e4f14e
Leekiahn <kiahn2000@gmail.com>
2026-07-25 07:08:39 +0900

    싱글톤 구현

---
+++ Runtime/Singleton/SingletonLazy.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace PhikozzLib
+{
+    public class SingletonLazy<T> : MonoBehaviour where T : Component
+    {
+        [SerializeField] private bool _dontDestroyOnLoad = true;
+        
+        private static T _instance;
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindAnyObjectByType<T>();
+                    
+                    if (_instance == null)
+                    {
+                        GameObject singletonObject = new GameObject(typeof(T).Name);
+                        _instance = singletonObject.AddComponent<T>();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            if (_instance == null)
+            {
+                _instance = this as T;
+            
+                if (_dontDestroyOnLoad)
+                {
+                    DontDestroyOnLoad(gameObject);
+                }
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+
+        }
+    }
+    
+}
