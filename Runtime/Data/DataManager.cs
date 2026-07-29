ce303b0eac2da6bb76a046afa825b3178243c06f
Leekiahn <kiahn2000@gmail.com>
2026-07-29 14:26:52 +0900

    폴더 정리

--- Runtime/Data/DataManager.cs
+++ Runtime/Data/DataManager.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using PhikozzLib;
 using UnityEngine;
 
 namespace PhikozzLib
--- Runtime/Localization/LocalTable/LocalizationTableRef.cs
+++ Runtime/Localization/LocalTable/LocalizationTableRef.cs
@@ -1,8 +1,8 @@
-﻿namespace PhikozzLib.LocalTable
+﻿namespace PhikozzLib
 {
     public static class LocalizationTableRef
     {
         // 오타 방지를 위해 Localization의 Table Reference를 상수로 정의합니다.
-        public const string TestTable = "Test Table";
+        public const string LocaleTable = "Locale Table";
     }
 }
