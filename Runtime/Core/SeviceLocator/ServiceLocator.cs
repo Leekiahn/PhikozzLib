f2128087937563c4ce12e7d02e15d4c9f06930e5
Leekiahn <kiahn2000@gmail.com>
2026-07-30 13:14:43 +0900

    Update ServiceLocator.cs

--- Runtime/Core/SeviceLocator/ServiceLocator.cs
+++ Runtime/Core/SeviceLocator/ServiceLocator.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 
 namespace PhikozzLib
 {
-    public static class ServiceLocator 
+    public static class ServiceLocator
     {
-        private static Dictionary<Type, object> _services = new Dictionary<Type, object>();
+        private static Dictionary<Type, object> _services = new();
 
         public static void Register<T>(T service) where T : class
         {
             _services[typeof(T)] = service;
+        }
+        
+        public static void Unregister<T>() where T : class
+        {
+            _services.Remove(typeof(T));
         }
     
         public static T Get<T>() where T : class
@@ -19,13 +24,7 @@ namespace PhikozzLib
                 return service as T;
             }
         
-            throw new Exception($"{typeof(T).Name} 서비스가 등록되지 않았습니다." + 
-                                "ServiceLocator를 통해서 서비스를 등록해주세요.");
-        }
-    
-        public static void Unregister<T>() where T : class
-        {
-            _services.Remove(typeof(T));
+            throw new Exception($"{typeof(T).Name} : it is not registered");
         }
     }
 }
