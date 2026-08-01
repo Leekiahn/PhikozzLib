e3070c9e6190607784493d39d0334ddc7332abab
Leekiahn <kiahn2000@gmail.com>
2026-08-01 10:55:37 +0900

    Update InputManager.cs

--- Runtime/Core/Input/InputManager.cs
+++ Runtime/Core/Input/InputManager.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace PhikozzLib
@@ -5,12 +6,15 @@ namespace PhikozzLib
     public class InputManager : MonoBehaviour, IServiceRegister
     {
         public PlayerInputAction ActionMaps { get; private set; }
-    
+
+        private void Awake()
+        {
+            ActionMaps = new PlayerInputAction();
+        }
+
         public void RegisterService()
         {
             ServiceLocator.Register(this);
-        
-            ActionMaps = new PlayerInputAction();
         }
 
         // ActionMaps의 액션을 사용하여 입력을 처리하는 메서드를 아래와 같이 추가할 수 있습니다.
